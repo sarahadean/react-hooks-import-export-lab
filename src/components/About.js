1dcfe363ac6c87b9d92e222b3580fe0422c796d0
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-04-30 22:41:45 -0500

    import and export

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import image from '/Users/sarahdean/Development/code/Phase-2/react-hooks-import-export-lab/src/data/user.js'
 
 function About() {
   return (
@@ -9,3 +10,5 @@ function About() {
     </div>
   );
 }
+
+export default About
